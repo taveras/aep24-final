828731417649daf268dd804269c85f21fdcb2869
Daniel Taveras <me@danieltaveras.com>
2024-04-16 23:01:59 -0700

    add Portfolio#buyStock

--- src/edu/berkeley/aep/Portfolio.java
+++ src/edu/berkeley/aep/Portfolio.java
@@ -1,8 +1,21 @@
 package edu.berkeley.aep;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class Portfolio {
+    private Map<Stock,Integer> stockQuantities = new HashMap<>();
     public Portfolio() {}
     public int currentValue() {
-        return 0;
+        int result = 0;
+        for(Stock s : stockQuantities.keySet()) {
+            result += s.currentPrice() * stockQuantities.get(s);
+        }
+        return result;
+    }
+
+    public void buyStock(Stock stockToBuy, int quantity) {
+        stockQuantities.put(stockToBuy, quantity);
     }
 }
--- test/edu/berkeley/aep/PortfolioTest.java
+++ test/edu/berkeley/aep/PortfolioTest.java
@@ -10,4 +10,12 @@ public class PortfolioTest {
         Portfolio p = new Portfolio();
         assertEquals(0, p.currentValue());
     }
+    @Test
+    public void shouldBeAbleToBuyStock() {
+        Portfolio p = new Portfolio();
+        Stock company = new Stock("XYZ", 50);
+
+        p.buyStock(company, 1);
+        assertEquals(50, p.currentValue());
+    }
 }
